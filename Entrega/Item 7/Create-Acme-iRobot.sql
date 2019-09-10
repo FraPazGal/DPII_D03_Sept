b256b667b37c04f042bc39209a4458c729b51a8e
FraPazGal <frandepaz@yahoo.es>
2019-09-10 03:53:23 +0200

    Primera versión entrega

---
+++ Entrega/Item 7/Create-Acme-iRobot.sql
@@ -0,0 +1,513 @@
+﻿start transaction;
+
+create database `Acme-iRobot`;
+
+use `Acme-iRobot`;
+
+create user 'acme-user'@'%' 
+	identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+
+create user 'acme-manager'@'%' 
+	identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete 
+	on `Acme-iRobot`.* to 'acme-user'@'%';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+    on `Acme-iRobot`.* to 'acme-manager'@'%';
+
+
+-- MySQL dump 10.13  Distrib 5.5.29, for Win64 (x86)
+--
+-- Host: localhost    Database: Acme-iRobot
+-- ------------------------------------------------------
+-- Server version	5.5.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `user_account` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_7ohwsa2usmvu0yxb44je2lge` (`user_account`),
+  CONSTRAINT `FK_7ohwsa2usmvu0yxb44je2lge` FOREIGN KEY (`user_account`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` VALUES (5,0,'C/ Collapsing Star Roaring Cannon','boros@','Boros','+34666666666','https://vignette.wikia.nocookie.net/onepunchman/images/c/ce/Boros_color.jpg/revision/latest?cb=20151017212925','Dominator of the Universe',4);
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `body` longtext,
+  `published_date` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `irobot` int(11) DEFAULT NULL,
+  `writer` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_5sntdvv4266brqnthg2i0c8e1` (`irobot`),
+  CONSTRAINT `FK_5sntdvv4266brqnthg2i0c8e1` FOREIGN KEY (`irobot`) REFERENCES `irobot` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `user_account` int(11) NOT NULL,
+  `vatnumber` varchar(255) DEFAULT NULL,
+  `cvv` int(11) DEFAULT NULL,
+  `expiration_month` int(11) DEFAULT NULL,
+  `expiration_year` int(11) DEFAULT NULL,
+  `holder` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_mbvdes9ypo1yu76so76owiyqx` (`user_account`),
+  CONSTRAINT `FK_mbvdes9ypo1yu76so76owiyqx` FOREIGN KEY (`user_account`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `finder`
+--
+
+DROP TABLE IF EXISTS `finder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `finder` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `key_word` varchar(255) DEFAULT NULL,
+  `maximum_price` double DEFAULT NULL,
+  `minimum_price` double DEFAULT NULL,
+  `search_moment` datetime DEFAULT NULL,
+  `customer` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_i9i5l2bcvnc58jsb1fbufcowo` (`customer`),
+  CONSTRAINT `FK_i9i5l2bcvnc58jsb1fbufcowo` FOREIGN KEY (`customer`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `finder`
+--
+
+LOCK TABLES `finder` WRITE;
+/*!40000 ALTER TABLE `finder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `finder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `finder_results`
+--
+
+DROP TABLE IF EXISTS `finder_results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `finder_results` (
+  `finder` int(11) NOT NULL,
+  `results` int(11) NOT NULL,
+  KEY `FK_a376m4iqwc9bvh4fqwqbt7f0i` (`results`),
+  KEY `FK_bo0nlx958mxty3aio68pb9bsq` (`finder`),
+  CONSTRAINT `FK_bo0nlx958mxty3aio68pb9bsq` FOREIGN KEY (`finder`) REFERENCES `finder` (`id`),
+  CONSTRAINT `FK_a376m4iqwc9bvh4fqwqbt7f0i` FOREIGN KEY (`results`) REFERENCES `irobot` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `finder_results`
+--
+
+LOCK TABLES `finder_results` WRITE;
+/*!40000 ALTER TABLE `finder_results` DISABLE KEYS */;
+/*!40000 ALTER TABLE `finder_results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequences`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequences` (
+  `sequence_name` varchar(255) DEFAULT NULL,
+  `sequence_next_hi_value` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequences`
+--
+
+LOCK TABLES `hibernate_sequences` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequences` DISABLE KEYS */;
+INSERT INTO `hibernate_sequences` VALUES ('domain_entity',1);
+/*!40000 ALTER TABLE `hibernate_sequences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `irobot`
+--
+
+DROP TABLE IF EXISTS `irobot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `irobot` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `description` longtext,
+  `is_decommissioned` bit(1) NOT NULL,
+  `is_deleted` bit(1) NOT NULL,
+  `price` double DEFAULT NULL,
+  `ticker` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `scientist` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_g5xb56esnn90ci3bdhk4wvlu2` (`scientist`),
+  CONSTRAINT `FK_g5xb56esnn90ci3bdhk4wvlu2` FOREIGN KEY (`scientist`) REFERENCES `scientist` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `irobot`
+--
+
+LOCK TABLES `irobot` WRITE;
+/*!40000 ALTER TABLE `irobot` DISABLE KEYS */;
+/*!40000 ALTER TABLE `irobot` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase`
+--
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `cvv` int(11) DEFAULT NULL,
+  `expiration_month` int(11) DEFAULT NULL,
+  `expiration_year` int(11) DEFAULT NULL,
+  `holder` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `purchase_moment` datetime DEFAULT NULL,
+  `customer` int(11) DEFAULT NULL,
+  `i_robot` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_hvb7dmfacqc4i0hp46dnr73pe` (`customer`),
+  KEY `FK_i7qhwod95yvy85qcsknfmnr96` (`i_robot`),
+  CONSTRAINT `FK_i7qhwod95yvy85qcsknfmnr96` FOREIGN KEY (`i_robot`) REFERENCES `irobot` (`id`),
+  CONSTRAINT `FK_hvb7dmfacqc4i0hp46dnr73pe` FOREIGN KEY (`customer`) REFERENCES `customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase`
+--
+
+LOCK TABLES `purchase` WRITE;
+/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `scientist`
+--
+
+DROP TABLE IF EXISTS `scientist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `scientist` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `user_account` int(11) NOT NULL,
+  `vatnumber` varchar(255) DEFAULT NULL,
+  `cvv` int(11) DEFAULT NULL,
+  `expiration_month` int(11) DEFAULT NULL,
+  `expiration_year` int(11) DEFAULT NULL,
+  `holder` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_5oysac5jumo3nk8evkbj3i6ll` (`user_account`),
+  CONSTRAINT `FK_5oysac5jumo3nk8evkbj3i6ll` FOREIGN KEY (`user_account`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `scientist`
+--
+
+LOCK TABLES `scientist` WRITE;
+/*!40000 ALTER TABLE `scientist` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scientist` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_configuration`
+--
+
+DROP TABLE IF EXISTS `system_configuration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `system_configuration` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `banner` varchar(255) DEFAULT NULL,
+  `country_code` varchar(255) DEFAULT NULL,
+  `makers` varchar(255) DEFAULT NULL,
+  `max_results` int(11) DEFAULT NULL,
+  `system_name` varchar(255) DEFAULT NULL,
+  `time_results_cached` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_configuration`
+--
+
+LOCK TABLES `system_configuration` WRITE;
+/*!40000 ALTER TABLE `system_configuration` DISABLE KEYS */;
+INSERT INTO `system_configuration` VALUES (6,0,'https://i.ibb.co/6Bpvwck/acme-irobot.png','+034','VISA,MASTERCARD,DINNERS,AMEX',10,'Acme-iRobot',5);
+/*!40000 ALTER TABLE `system_configuration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_configuration_breach_notification`
+--
+
+DROP TABLE IF EXISTS `system_configuration_breach_notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `system_configuration_breach_notification` (
+  `system_configuration` int(11) NOT NULL,
+  `breach_notification` varchar(255) DEFAULT NULL,
+  `breach_notification_key` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`system_configuration`,`breach_notification_key`),
+  CONSTRAINT `FK_ayrxtmu4hqnpdg16dr66ftfun` FOREIGN KEY (`system_configuration`) REFERENCES `system_configuration` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_configuration_breach_notification`
+--
+
+LOCK TABLES `system_configuration_breach_notification` WRITE;
+/*!40000 ALTER TABLE `system_configuration_breach_notification` DISABLE KEYS */;
+/*!40000 ALTER TABLE `system_configuration_breach_notification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_configuration_welcome_message`
+--
+
+DROP TABLE IF EXISTS `system_configuration_welcome_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `system_configuration_welcome_message` (
+  `system_configuration` int(11) NOT NULL,
+  `welcome_message` varchar(255) DEFAULT NULL,
+  `welcome_message_key` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`system_configuration`,`welcome_message_key`),
+  CONSTRAINT `FK_afo2sg9l0wrwhjs6s7qjxwgjg` FOREIGN KEY (`system_configuration`) REFERENCES `system_configuration` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_configuration_welcome_message`
+--
+
+LOCK TABLES `system_configuration_welcome_message` WRITE;
+/*!40000 ALTER TABLE `system_configuration_welcome_message` DISABLE KEYS */;
+INSERT INTO `system_configuration_welcome_message` VALUES (6,'Welcome to Acme iRobot!Welcome to a new era of computing','English'),(6,'¡Bienvenidos a Acme iRobot! ¡Bienvenidos a una nueva era de la computación!','Español');
+/*!40000 ALTER TABLE `system_configuration_welcome_message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `user_account` int(11) NOT NULL,
+  `vatnumber` varchar(255) DEFAULT NULL,
+  `cvv` int(11) DEFAULT NULL,
+  `expiration_month` int(11) DEFAULT NULL,
+  `expiration_year` int(11) DEFAULT NULL,
+  `holder` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_cjn1wn3ecn1kacgqxryr6a5c6` (`user_account`),
+  CONSTRAINT `FK_cjn1wn3ecn1kacgqxryr6a5c6` FOREIGN KEY (`user_account`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_account` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `is_banned` bit(1) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_castjbvpeeus0r8lbpehiu0e4` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES (4,0,'\0','21232f297a57a5a743894a0e4a801fc3','admin');
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_account_authorities`
+--
+
+DROP TABLE IF EXISTS `user_account_authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_account_authorities` (
+  `user_account` int(11) NOT NULL,
+  `authority` varchar(255) DEFAULT NULL,
+  KEY `FK_pao8cwh93fpccb0bx6ilq6gsl` (`user_account`),
+  CONSTRAINT `FK_pao8cwh93fpccb0bx6ilq6gsl` FOREIGN KEY (`user_account`) REFERENCES `user_account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account_authorities`
+--
+
+LOCK TABLES `user_account_authorities` WRITE;
+/*!40000 ALTER TABLE `user_account_authorities` DISABLE KEYS */;
+INSERT INTO `user_account_authorities` VALUES (4,'ADMIN');
+/*!40000 ALTER TABLE `user_account_authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-10  3:46:05
+
+commit;
